fa9fe20d1df38ef774948fb8f6dbba29540f35ad
miguecarrera <ahola6479@gmail.com>
2022-02-10 09:42:01 +0100

    Despliegue de Heroku

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1n$5os)^5acljr1*#w7(sh84(yr93!yxd)r!*1jnwib$hfjcw=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
